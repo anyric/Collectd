56969e91ec554e60b7b1f4050a86a0224f5718e5
anyric <anyamaronyango@gmail.com>
2018-09-18 18:24:30 +0300

    add user feedback to functions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 install_tools() {
+  echo "Installing tools"
   sudo apt-get update && \
   sudo apt-get install -y sysstat collectd \
     librrds-perl libjson-perl libhtml-parser-perl
 }
 
 configure_sysstat() {
+  echo "Configuring sysstat"
   sudo rm -rf /etc/default/sysstat
   sudo cp sysstat.conf /etc/default/sysstat
 }
 
 configure_collectd() {
+  echo "Configuring Collectd"
   sudo service collectd start
 
   cd /usr/local/
-  git clone https://github.com/httpdss/collectd-web.git
+  sudo git clone https://github.com/httpdss/collectd-web.git
   cd collectd-web/
   chmod +x cgi-bin/graphdefs.cgi
 }
 
 configure_appache2() {
+  echo "Configuring apache2"
   sudo cat apache2.conf >> /etc/apache2/sites-available/000-default.conf
 
   sudo a2enmod cgi cgid
@@ -28,6 +32,7 @@ configure_appache2() {
 }
 
 start_cgi_server() {
+  echo "Starting CGI Server"
   cd collectd-web/
   sudo ./runserver.py &
 }
